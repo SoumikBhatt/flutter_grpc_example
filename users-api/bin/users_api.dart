cec7c2ffb4998c5bb29830c53a5afc91468e2041
soumikBhatt <soumikcse07@gmail.com>
2023-05-31 18:40:16 +0600

    feat: server side code added for gRPC client

---
+++ users-api/bin/users_api.dart
@@ -0,0 +1,5 @@
+import 'package:users_api/users_api.dart' as users_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${users_api.calculate()}!');
+}
---
+++ users-api/test/users_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:users_api/users_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
